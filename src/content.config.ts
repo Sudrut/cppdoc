f8a09537c1b93d313a798f17db3d75de6f38abbb
Sirui Mu <msrlancern@gmail.com>
2025-11-27 21:22:58 +0800

    feat: initial implementation of the doc index system

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,11 +1,22 @@
-import { defineCollection } from "astro:content";
+import { defineCollection, z } from "astro:content";
 import { docsLoader } from "@astrojs/starlight/loaders";
 import { docsSchema } from "@astrojs/starlight/schema";
 import { autoSidebarLoader } from "starlight-auto-sidebar/loader";
 import { autoSidebarSchema } from "starlight-auto-sidebar/schema";
 
 export const collections = {
-  docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
+  docs: defineCollection({
+    loader: docsLoader(),
+    schema: docsSchema({
+      extend: z.object({
+        cppdoc: z
+          .object({
+            keys: z.array(z.string()).optional(),
+          })
+          .optional(),
+      }),
+    }),
+  }),
   autoSidebar: defineCollection({
     loader: autoSidebarLoader(),
     schema: autoSidebarSchema(),
